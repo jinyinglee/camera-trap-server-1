d9e17ddbc570cfce5120706c4ff1acfef01d01ba
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-07-09 18:01:44 +0800

    updated base.html & project overview

--- taicat/urls.py
+++ taicat/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('project/overview', views.overview, name='overview'),
+    path('project/create', views.create_project, name='create_project'),
     path('project/<pk>/', views.project_detail, name='project_detail'),
     path('api/data', views.data, name='data')
 ]
