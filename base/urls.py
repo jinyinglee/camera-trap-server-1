479da811a8bdb2ad98a6ccd3761cea81a0a4596c
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-07-14 17:33:42 +0800

    altered loading process of homepage & set project pages

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('api/stat_county', views.stat_county, name='stat_county')
+    path('api/stat_county', views.stat_county, name='stat_county'),
+    path('api/get_home_data', views.get_home_data, name='get_home_data')
 ]
