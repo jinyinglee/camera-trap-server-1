83429d4d55ca7c3ceaf43bcc1ac716ed3cc847fc
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-07-14 09:54:43 +0800

    added homepage & part of project pages

--- taicat/urls.py
+++ taicat/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('project/overview', views.overview, name='overview'),
     path('project/create', views.create_project, name='create_project'),
-    path('project/<pk>/', views.project_detail, name='project_detail'),
+    path('project/edit/basic/<pk>/', views.edit_project_basic, name='edit_project_basic'),
+    path('project/details/<pk>/', views.project_detail, name='project_detail'),
     path('api/data', views.data, name='data')
 ]
