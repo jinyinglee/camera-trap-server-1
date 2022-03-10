61bc93759758703dc39f304305fe10ccd8e4daf4
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2022-03-10 16:46:00 +0800

    feature: faq page

--- base/urls.py
+++ base/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('permission', views.set_permission, name='set_permission'),
     path('add_org_admin', views.add_org_admin, name='add_org_admin'),
     path('policy', views.policy, name='policy'),
+    path('faq', views.faq, name='faq'),
+    path('contact-us', views.contact_us, name='contact-us'),
 ]
