cfe53e301132bb0c1806aa4c8fc5868f111ca1cd
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2022-05-04 09:43:00 +0800

    [SCHEMA] add upload history table

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from taicat.models import DeploymentJournal
 
-# Create your models here.
+
+class UploadHistory(models.Model):
+    STATUS_CHOICES = (
+        ('uploading', '上傳中'),
+        ('finished', '已完成'),
+        ('unfinished', '未完成'),
+    )
+    last_updated = models.DateTimeField(null=True, db_index=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, null=True, blank=True)
+    deployment_journal = models.ForeignKey(DeploymentJournal, on_delete=models.SET_NULL, null=True, blank=True) # 知道是那次上傳的
