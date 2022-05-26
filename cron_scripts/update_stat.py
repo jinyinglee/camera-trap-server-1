ea4e859e0d4c5faa4d05ed6d037232d64a2236b9
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2022-05-26 14:52:02 +0800

    add homepage map stat

--- cron_scripts/update_stat.py
+++ cron_scripts/update_stat.py
@@ -152,3 +152,166 @@ for p in Project.objects.all().values('id'):
                 folder_last_updated=f_last_updated,
                 project_id=p['id'])
             img_f.save()
+
+# ---------- HOMEPAGE MAP ----------- #
+import geopandas as gpd
+from conf.settings import BASE_DIR
+import os
+import pygeos
+gpd.options.use_pygeos = True
+
+print('start HOMEPAGE MAP', now)
+
+species_list = ['水鹿', '山羌', '獼猴', '山羊', '野豬', '鼬獾', '白鼻心', '食蟹獴', '松鼠',
+                '飛鼠', '黃喉貂', '黃鼠狼', '小黃鼠狼', '麝香貓', '黑熊', '石虎', '穿山甲', '梅花鹿', '野兔', '蝙蝠']
+
+now = timezone.now()
+
+gpd.options.use_pygeos = True
+
+geo_df = gpd.read_file(os.path.join(os.path.join(BASE_DIR, "static"),'map/COUNTY_MOI_1090820.shp'))
+
+# 只選擇正式的資料
+
+query = """
+        SELECT d.longitude, d.latitude, d.id, d.geodetic_datum FROM taicat_deployment d
+        JOIN taicat_project p ON d.project_id = p.id
+        WHERE p.mode = 'official';
+        """
+
+with connection.cursor() as cursor:
+    cursor.execute(query)
+    d_df = cursor.fetchall()
+    d_df = pd.DataFrame(d_df, columns=['longitude', 'latitude', 'did', 'geodetic_datum'])
+
+
+# d_df = pd.DataFrame(Deployment.objects.all().values('longitude','latitude','id', 'geodetic_datum'))
+
+d_gdf = gpd.GeoDataFrame(d_df,geometry=gpd.points_from_xy(d_df.longitude,d_df.latitude))
+join = gpd.sjoin(geo_df, d_gdf)
+
+# TWD97經緯度=WGS84 ?
+
+county = geo_df.COUNTYNAME.unique()
+
+for c in county:
+    # print(c)
+    d_list = join[join['COUNTYNAME']==c].did.unique()
+    d_list_str = ",".join([str(x) for x in d_list])
+    num_project = 0
+    num_deployment = 0
+    num_image = 0
+    num_working_hour = 0
+    identified = 0
+    species_str = ''
+    sa_str = ''
+    if len(d_list):
+        query = f"SELECT DISTINCT(studyarea_id) FROM taicat_image WHERE deployment_id IN ({d_list_str})"
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            sa = cursor.fetchall()
+            sa = [str(s[0]) for s in sa]
+            sa_str = ','.join(sa)
+        query = f"""SELECT COUNT(DISTINCT(image_uuid)) FROM taicat_image WHERE species IS NOT NULL and species != '' and deployment_id IN ({d_list_str}) ;"""
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            identified = cursor.fetchall()
+        query = f"""
+                SELECT COUNT(DISTINCT(d.project_id)), SUM(ds.count_working_hour)
+                FROM taicat_deployment d 
+                LEFT JOIN taicat_deploymentstat ds ON d.id = ds.deployment_id
+                WHERE d.id IN ({d_list_str});
+                """
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            stat = cursor.fetchall()
+        query = f"""
+                SELECT COUNT(DISTINCT(image_uuid)) 
+                FROM taicat_image 
+                WHERE deployment_id IN ({d_list_str});
+                """
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            num_image = cursor.fetchall()
+        query = f"""SELECT DISTINCT(species) FROM taicat_image WHERE deployment_id IN ({d_list_str})"""
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            species = cursor.fetchall()
+            species = [s[0] for s in species if s[0] in species_list]
+            species_str = ','.join(species)        
+        num_project = stat[0][0]
+        num_deployment = len(d_list)
+        num_image = num_image[0][0]
+        num_working_hour = stat[0][1] if stat[0][1] else 0
+        identified = round((identified[0][0] / num_image) * 100, 2)
+    # else:
+    #     # 沒有該縣市的資料，填0
+    #     num_project = 0
+    #     num_deployment = 0
+    #     num_image = 0
+    #     num_working_hour = 0
+    #     identified = 0
+    #     species_str = ''
+    # 沒有的話新增
+    # 有的話更新
+    if GeoStat.objects.filter(county=c).exists():
+        GeoStat.objects.filter(county=c).update(
+            num_project = num_project,
+            num_deployment = num_deployment,
+            num_image = num_image,
+            num_working_hour = num_working_hour,
+            identified = identified,
+            species = species_str,
+            studyarea = sa_str,
+            last_updated = now
+        )
+    else:
+        GeoStat.objects.create(
+            county = c,
+            num_project = num_project,
+            num_deployment = num_deployment,
+            num_image = num_image,
+            num_working_hour = num_working_hour,
+            identified = identified,
+            species = species_str,
+            studyarea = sa_str
+        )
+    
+
+# center of studyarea
+# 
+query = """
+        SELECT d.longitude, d.latitude, d.id, d.study_area_id FROM taicat_deployment d
+        JOIN taicat_project p ON d.project_id = p.id
+        WHERE p.mode = 'official';
+        """
+
+with connection.cursor() as cursor:
+    cursor.execute(query)
+    sa_df = cursor.fetchall()
+    sa_df = pd.DataFrame(sa_df, columns=['longitude', 'latitude', 'did', 'said'])
+
+# d_df = pd.DataFrame(Deployment.objects.all().values('longitude','latitude','id', 'geodetic_datum'))
+
+sa_gdf = gpd.GeoDataFrame(sa_df,geometry=gpd.points_from_xy(sa_df.longitude,sa_df.latitude))
+sa_list = sa_df.said.unique()
+
+for i in sa_list:
+    # print(i)
+    # print(i, sa_gdf[sa_gdf['said']==i].dissolve().centroid)
+    long = sa_gdf[sa_gdf['said']==i].dissolve().centroid.x[0]
+    lat = sa_gdf[sa_gdf['said']==i].dissolve().centroid.y[0]
+    if StudyAreaStat.objects.filter(studyarea_id=i).exists():
+        StudyAreaStat.objects.filter(studyarea_id=i).update(
+            longitude = long,
+            latitude = lat,
+            last_updated = now
+        )
+    else:
+        StudyAreaStat.objects.create(
+            studyarea_id = i,
+            longitude = long,
+            latitude = lat,
+        )
+
+
