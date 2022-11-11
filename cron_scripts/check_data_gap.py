bf5163917022a70f31f0e065083768f7e836dc9f
MooGoo <moogoo78@gmail.com>
2022-11-11 11:28:39 +0800

    fix: check_data_gap crontab script
    update

--- cron_scripts/check_data_gap.py
+++ cron_scripts/check_data_gap.py
@@ -18,46 +18,40 @@ from base.models import (
 now = datetime.datetime.now()
 
 # test
-# range_list = half_year_ago(2017, 6)
-
+#range_list = half_year_ago(2021, 4)
 range_list = half_year_ago(now.year, now.month)
-rows = DeploymentJournal.objects.values_list('project_id', 'working_start__year').annotate(total=Count('id')).filter(working_end__gt=range_list[0], working_start__lt=range_list[1]).all()
+rows = DeploymentJournal.objects.values_list('project_id').annotate(total=Count('id')).filter(working_end__gt=range_list[0]).all()
+
+# rows = [[141]]
 
 for i in rows:
     project_id = i[0]
-    year = i[1]
     proj = Project.objects.get(pk=project_id)
-    data = proj.count_deployment_journal([year])
-
-
+    results = proj.count_deployment_journal([range_list[1].year-1, range_list[1].year])
     output = {'count': 0, 'studyareas': {}}
-    for sa in data[str(year)]:
-        # body += '\n\n# 樣區名稱: {}\n'.format(sa['name'])
-        #sa_title = '\n\n# 樣區名稱: {}\n'.format(sa['name'])
-        sa_output = {'count': 0, 'deployments': {}}
-        for dep in sa['items']:
-            #dep_title = '\n## 相機位置: {}\n'.format(dep['name'])
-            for gap in dep['gaps']:
-                gap_start = datetime.datetime.fromtimestamp(gap['range'][0])
-                gap_end = datetime.datetime.fromtimestamp(gap['range'][1])
-                #print(range_list, gap_start, gap_end)
-                if gap_end >= range_list[0] or gap_start <= range_list[1]:
-                    if dep['id'] not in sa_output['deployments']:
-                        sa_output['deployments'][dep['id']] = {
-                            'count': 0,
-                            'items': [],
-                            'name': dep['name']
-                        }
+    for year, data in results.items():
+        for sa in data:
+            for dep in sa['items']:
+                for gap in dep['gaps']:
+                    gap_start = datetime.datetime.fromtimestamp(gap['range'][0])
+                    gap_end = datetime.datetime.fromtimestamp(gap['range'][1])
+                    #print(range_list, gap_start, gap_end)
+                    if gap_end >= range_list[0] and gap_start <= range_list[1]:
+                        if sa['name'] not in output['studyareas']:
+                            output['studyareas'][sa['name']] = {}
 
-                    gap_title = '* {}/{}\n'.format(gap_start.strftime('%Y-%m-%d'), gap_end.strftime('%Y-%m-%d'))
-                    sa_output['deployments'][dep['id']]['items'].append(gap_title)
-                    sa_output['deployments'][dep['id']]['count'] += 1
-                    sa_output['count'] += 1
+                        if dep['id'] not in output['studyareas'][sa['name']]:
+                            output['studyareas'][sa['name']][dep['id']] = {
+                               'items': [],
+                               'name': dep['name'],
+                            }
+                            output['count'] += 1
 
-            output['count'] += sa_output['count']
-            output['studyareas'][sa['name']] = sa_output['deployments']
+                        gap_title = '* {}/{}\n'.format(gap_start.strftime('%Y-%m-%d'), gap_end.strftime('%Y-%m-%d'))
+                        # print(year, dep['name'], gap_title)
+                        output['studyareas'][sa['name']][dep['id']]['items'].append(gap_title)
 
-    # print(output)
+    #print(output)
 
     email_subject = '[臺灣自動相機資訊系統] | {} | 資料缺失: 尚未填寫列表 | 篩選範圍：{}/{}'.format(proj.name, range_list[0].strftime('%Y-%m-%d'), range_list[1].strftime('%Y-%m-%d'))
     email_body = '資料缺失: 尚未填寫列表\n----------------------'
@@ -85,5 +79,7 @@ for i in rows:
         un.save()
 
     send_mail(email_subject, email_body, settings.CT_SERVICE_EMAIL, email_list)
+
     #print(email_subject)
     #print(email_body)
+
