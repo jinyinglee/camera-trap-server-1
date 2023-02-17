24f33de88498a2045b63954aa1a034f6463428f4
MooGoo <moogoo78@gmail.com>
2023-02-17 11:31:22 +0800

    fix: keep client_view.post_image_annotation as v1, and add updated v1.1

--- taicat/client_views.py
+++ taicat/client_views.py
@@ -80,6 +80,95 @@ def get_project(request, project_id):
 def post_image_annotation(request):
     ret = {}
     if request.method == 'POST':
+        data = json.loads(request.body)
+
+        deployment = Deployment.objects.get(pk=data['deployment_id'])
+        if deployment:
+            res = {}
+            # aware datetime object
+            utc_tz = pytz.timezone(settings.TIME_ZONE)
+
+            # find if is specific_bucket
+            bucket_name = data.get('bucket_name', '')
+            specific_bucket = ''
+            if bucket_name != settings.AWS_S3_BUCKET:
+                specific_bucket = bucket_name
+
+            folder_name = ''
+
+
+            folder_name = data['folder_name']
+
+            # create or update DeploymentJournal
+            deployment_journal_id = set_deployment_journal(data, deployment)
+
+
+            for i in data['image_list']:
+                img_info_payload = None
+                # prevent json load error
+                exif_str = i[9].replace('\\u0000', '') if i[9] else '{}'
+                exif = json.loads(exif_str)
+                anno = json.loads(i[7]) if i[7] else '{}'
+                if i[11]:
+                    is_new_image = False
+                    img = Image.objects.get(pk=i[11])
+                    # only update annotation
+                    img.annotation = anno
+                    img.last_updated = datetime.now()
+                else:
+                    image_uuid = str(ObjectId())
+                    img = Image(
+                        deployment_id=deployment.id,
+                        filename=i[2],
+                        datetime=datetime.fromtimestamp(i[3], utc_tz),
+                        image_hash=i[6],
+                        annotation=anno,
+                        memo=data['key'],
+                        image_uuid=image_uuid,
+                        has_storage='N',
+                        folder_name=folder_name,
+                    )
+
+                    if deployment_journal_id != '':
+                        img.deployment_journal_id = deployment_journal_id
+                    if specific_bucket != '':
+                        img.specific_bucket = specific_bucket
+
+                    img_info_payload = {
+                        'source_data': i,
+                        'exif': exif,
+                        'image_uuid': image_uuid
+                    }
+                    if pid := deployment.project_id:
+                        img.project_id = pid
+                    if said := deployment.study_area_id:
+                        img.studyarea_id = said
+
+                img.save()
+                res[i[0]] = [img.id, img.image_uuid]
+
+                set_image_annotation(img)
+
+                if img_info_payload != None:
+                    # seperate image_info
+                    img_info = Image_info(
+                        image_uuid=img_info_payload['image_uuid'],
+                        source_data=img_info_payload['source_data'],
+                        exif=img_info_payload['exif'],
+                    )
+                    img_info.save()
+
+            ret['saved_image_ids'] = res
+            ret['deployment_journal_id'] = deployment_journal_id
+        else:
+            ret['error'] = 'ct-server: no deployment key'
+
+    return JsonResponse(ret)
+
+@csrf_exempt
+def post_image_annotation1_1(request):
+    ret = {}
+    if request.method == 'POST':
         data = {}
 
         # upload zipped file instead of json body
