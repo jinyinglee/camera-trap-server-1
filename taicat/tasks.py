8b03d8360bbf5f474236c13bef8bcee6e40af840
MooGoo <moogoo78@gmail.com>
2023-05-03 18:57:40 +0800

    fix: download save to download_log & calc download use celery and email

--- taicat/tasks.py
+++ taicat/tasks.py
@@ -1,14 +1,22 @@
 import pytz
 import json
 from datetime import datetime
+import csv
+from pathlib import Path
+
 from bson.objectid import ObjectId
 
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
-from taicat.utils import save_calculation
+from taicat.utils import (
+    save_calculation,
+    apply_search_filter,
+)
 
 logger = get_task_logger(__name__)
 
@@ -17,10 +25,15 @@ from taicat.models import (
     Image_info,
     Deployment,
     DeploymentJournal,
+    DownloadLog,
+    ParameterCode,
+    Contact,
 )
 from base.models import UploadHistory
 from .utils import (
     set_image_annotation,
+    calculated_data,
+    calc_output,
 )
 
 @shared_task
@@ -121,3 +134,74 @@ def process_image_annotation_task(deployment_journal_id, data):
     deployment_journal.save()
 
     save_calculation(species_list, datetime_from, datetime_to, deployment_journal.deployment)
+
+
+@shared_task
+def process_download_data_task(email, filter_dict, member_id, host):
+    download_dir = Path(settings.MEDIA_ROOT, 'download')
+    filename = f'download_{str(ObjectId())}_{datetime.now().strftime("%Y-%m-%d")}.csv'
+    query = apply_search_filter(filter_dict)
+    header = ['計畫ID', '計畫名稱', '影像ID', '樣區/子樣區', '相機位置', '檔名', '拍攝時間', '物種', '年齡', '性別', '角況', '個體ID', '備註']
+    with open(Path(download_dir, filename), 'w') as csvfile:
+        spamwriter = csv.writer(csvfile)
+        spamwriter.writerow(header)
+        for i in query.all():
+            row = [
+                i.project_id,
+                i.project.name,
+                i.image_uuid,
+                i.studyarea.name,
+                i.deployment.name,
+                i.filename,
+                i.datetime.strftime('%Y-%m-%d %H:%M:%S') if i.datetime else '',
+                i.species,
+                i.life_stage,
+                i.sex,
+                i.antler,
+                i.animal_id,
+                i.remarks
+            ]
+            spamwriter.writerow(row)
+
+    download_url = "https://{}{}{}".format(
+        host,
+        settings.MEDIA_URL,
+        Path('download', filename))
+
+    user_role = ''
+    if contact := Contact.objects.get(id=member_id):
+        if role := ParameterCode.objects.filter(parametername=contact.identity).first():
+            user_role = role.name
+
+    # download_lo
+        #condiction_log = f'''專案名稱:{project_name}, 日期：{date_filter}。樣區 / 相機位置：{conditions} 。物種：{spe_conditions} 。時間：{time_filter}。縣市：{county_filter}。保護留區：{protectarea_filter}。資料夾：{folder_filter} 。'''
+    download_log_sql = DownloadLog(
+        user_role=user_role,
+        condiction=json.dumps(filter_dict),
+        file_link=download_url)
+    download_log_sql.save()
+
+    email_subject = '[臺灣自動相機資訊系統] 下載資料'
+    email_body = render_to_string('project/download.html', {'download_url': download_url, })
+    send_mail(email_subject, email_body, settings.CT_SERVICE_EMAIL, [email])
+
+
+@shared_task
+def process_download_calculated_data_task(email, filter_dict, calc_dict, calc_type, out_format, calc_data, host):
+    results = calculated_data(filter_dict, calc_data)
+    content = calc_output(results, out_format, json.dumps(filter_dict), json.dumps(calc_data))
+
+    ext = 'csv' if out_format == 'csv' else 'xlsx'
+    filename = f'download_calculated_{str(ObjectId())}_{datetime.now().strftime("%Y-%m-%d")}.{ext}'
+    download_dir = Path(settings.MEDIA_ROOT, 'download')
+    with open(Path(download_dir, filename), 'wb') as outfile:
+        outfile.write(content)
+
+    download_url = "https://{}{}{}".format(
+        host,
+        settings.MEDIA_URL,
+        Path('download', filename))
+    email_subject = '[臺灣自動相機資訊系統] 下載計算資料'
+    email_body = render_to_string('project/download.html', {'download_url': download_url, })
+    #print('email', email_subject, email_body)
+    send_mail(email_subject, email_body, settings.CT_SERVICE_EMAIL, [email])
