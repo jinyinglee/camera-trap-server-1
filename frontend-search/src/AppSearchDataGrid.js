7fc4b9c868556294a5bafb6571eefd17b4bcc3d5
MooGoo <moogoo78@gmail.com>
2023-07-03 18:09:24 +0800

    style: project-oversight & search new layout (pagination)

--- frontend-search/src/AppSearchDataGrid.js
+++ frontend-search/src/AppSearchDataGrid.js
@@ -63,6 +63,58 @@ const columns = [
 const CellContent = (value) => {
 }
 
+const CustomTablePagination = ({count, rowsPerPage, rowsPerPageOptions, page, onPageChange, onRowsPerPageChange}) => {
+  console.log(count, rowsPerPage, page, rowsPerPageOptions);
+  const start = (page) * rowsPerPage;
+  const end = (page+1) * rowsPerPage;
+
+  return (
+    <td colSpan="9">
+      <div className="search-pagination-box">
+        <div className="search-pagination-flex">
+	  <span className="search-pagination-label">一頁幾筆</span>
+	  <div className="search-pagination-input">
+	    <select name="" id="" className="search-pagination-select" onChange={onRowsPerPageChange}>
+              {rowsPerPageOptions.map((option, index) => {
+                return (<option value={ option } key={ index }>{ option }</option>)
+              })}
+	    </select>
+	  </div>
+          <span className="search-pagination-description">{ `${start}-${end} of ${count}` }</span>
+          <button className="search-pagination-arrl" onClick={(e) => (page > 0)  ? onPageChange(e, page-1) : null}>
+	    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+	      <g id="Group_800" data-name="Group 800" transform="translate(-1050 -1644)">
+		<g id="Ellipse_6" data-name="Ellipse 6" transform="translate(1050 1644)" fill="#fff" stroke="#257455" strokeWidth="1">
+		  <circle cx="20" cy="20" r="20" stroke="none"></circle>
+		  <circle cx="20" cy="20" r="19.5" fill="none"></circle>
+		</g>
+		<g id="Group_721" data-name="Group 721" transform="translate(1067 1657)">
+		  <line id="Line_6" data-name="Line 6" x1="6.549" y2="6.549" transform="translate(0)" fill="none" stroke="#257455" strokeLinecap="round" strokeWidth="2"></line>
+		  <line id="Line_7" data-name="Line 7" x1="6.549" y1="6.549" transform="translate(0 6.549)" fill="none" stroke="#257455" strokeLinecap="round" strokeWidth="2"></line>
+		</g>
+	      </g>
+	    </svg>
+	  </button>
+          <button className="search-pagination-arrr" onClick={(e) => (page < (Math.ceil(count/rowsPerPage) - 1)) ? onPageChange(e, page+1) : null}>
+	    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+	      <g id="Group_801" data-name="Group 801" transform="translate(-1100 -1644)">
+		<g id="Ellipse_7" data-name="Ellipse 7" transform="translate(1100 1644)" fill="#fff" stroke="#257455" strokeWidth="1">
+		  <circle cx="20" cy="20" r="20" stroke="none"></circle>
+		  <circle cx="20" cy="20" r="19.5" fill="none"></circle>
+		</g>
+		<g id="Group_722" data-name="Group 722" transform="translate(-37.775 -28.049)">
+		  <line id="Line_6" data-name="Line 6" x2="6.549" y2="6.549" transform="translate(1154.5 1685.5)" fill="none" stroke="#257455" strokeLinecap="round" strokeWidth="2"></line>
+		  <line id="Line_7" data-name="Line 7" y1="6.549" x2="6.549" transform="translate(1154.5 1692.049)" fill="none" stroke="#257455" strokeLinecap="round" strokeWidth="2"></line>
+		</g>
+	      </g>
+	    </svg>
+	  </button>
+        </div>
+      </div>
+    </td>
+  )
+}
+
 const AppSearchDataGrid = ({result, pagination, handleChangePage, handleChangeRowsPerPage, setImageDetail}) => {
   // console.log('result', result);
   return (
@@ -112,6 +164,24 @@ const AppSearchDataGrid = ({result, pagination, handleChangePage, handleChangeRo
         </TableBody>
         <TableFooter>
           <TableRow>
+            <CustomTablePagination
+              rowsPerPageOptions={[20, 50, 100]}
+              count={result.total}
+              rowsPerPage={pagination.perPage}
+              page={pagination.pageIndex}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export { AppSearchDataGrid };
+
+/**
             <TablePagination
               rowsPerPageOptions={[20, 50, 100]}
               count={result.total}
@@ -126,12 +196,5 @@ const AppSearchDataGrid = ({result, pagination, handleChangePage, handleChangeRo
               labelRowsPerPage='一頁幾筆'
               onPageChange={handleChangePage}
               onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
-  );
-}
-
-export { AppSearchDataGrid };
+              />
+*/
